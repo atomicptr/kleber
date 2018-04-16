921bbbfe3bae13c3e32c819d3ed17dce0ec8a1d3
Christopher Kaster <me@atomicptr.de>
2018-04-16 15:23:31 +0200

    change scroll to anchor to an element instead of scroll number

--- src/main.js
+++ src/main.js
@@ -1,52 +1,58 @@
 "use strict";
 
 function kleberAll(query, func) {
-    document.querySelectorAll(query).forEach(func);
+    document.querySelectorAll(query).forEach(elem => {
+        if(elem.dataset.kleberTarget) {
+            return func(document.querySelector(elem.dataset.kleberTarget), elem);
+        }
+
+        func(elem, elem);
+    });
 }
 
 window.addEventListener("load", event => {
 
-    // register click handler
-    kleberAll("[data-kleber-click]", elem => {
-        elem.addEventListener("click", event => {
-            let target = elem;
-
-            if(elem.dataset.kleberTarget) {
-                target = document.querySelector(elem.dataset.kleberTarget);
-            }
-
-            const classes = elem.dataset.kleberClick.split(",");
+    // register click handlers
+    kleberAll("[data-kleber-click]", (target, source) => {
+        source.addEventListener("click", event => {
+            const classes = source.dataset.kleberClick.split(",");
             classes.forEach(className => target.classList.toggle(className));
         });
     });
 
-    kleberAll("[data-kleber-scroll]", elem => {
-        if(elem.dataset.kleberScrollThreshold) {
-            const checkScroll = event => {
-                if(!event) event = {}
+    // register scroll handlers
+    kleberAll("[data-kleber-scroll]", (target, source) => {
+        if(!window.__kleberScrollElements) {
+            window.__kleberScrollElements = [];
+        }
 
-                let target = event.target ? event.target.scrollingElement : {scrollTop: window.pageYOffset};
-
-                if(elem.dataset.kleberTarget) {
-                    target = document.querySelector(elem.dataset.kleberTarget);
-                }
-
-                const scrollTop = target.scrollTop;
-                const threshold = Number(elem.dataset.kleberScrollThreshold);
-                const classes = elem.dataset.kleberScroll.split(",");
-
-                if(scrollTop >= threshold) {
-                    classes.forEach(className => elem.classList.add(className));
-                } else {
-                    classes.forEach(className => elem.classList.remove(className));
-                }
-            };
-
-            window.addEventListener("scroll", checkScroll);
-
-            checkScroll();
+        if(source.dataset.kleberScrollTarget) {
+            window.__kleberScrollElements.push([target, source]);
         } else {
-            console.warn("Kleber: Scroll defined without scroll threshold, ignoring.");
+            console.warn("Kleber: Scroll defined without scroll target, ignoring.", source);
         }
     });
+
+    function checkScrollEvents() {
+        for(let [target, source] of window.__kleberScrollElements) {
+            const scrollBarTop = window.pageYOffset;
+
+            let targetElement = document.querySelector(source.dataset.kleberScrollTarget);
+            const classes = source.dataset.kleberScroll.split(",");
+
+            const scrollOffset = source.dataset.kleberScrollOffset ?
+                Number(source.dataset.kleberScrollOffset) : 0;
+
+            const targetOffset = targetElement.offsetTop;
+
+            if(scrollBarTop >= (targetOffset + scrollOffset)) {
+                classes.forEach(className => target.classList.add(className));
+            } else {
+                classes.forEach(className => target.classList.remove(className));
+            }
+        }
+    }
+
+    window.addEventListener("scroll", checkScrollEvents);
+    checkScrollEvents();
 })
